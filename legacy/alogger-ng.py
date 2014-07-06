fb08d3afd2c1a0d553afb498c68d8eabb52310d3
Brian May <brian@microcomaustralia.com.au>
2014-07-06 14:14:11 +1000

    Fix copyright notices.
    Change-Id: I65aa0565cb1eb29ec1ab5f9196b6a86cbce25611

--- legacy/alogger-ng.py
+++ legacy/alogger-ng.py
@@ -1,11 +1,28 @@
 #! /usr/bin/env python
 
+# Copyright 208-2014 VPAC
+#
+# This file is part of python-alogger.
+#
+# python-alogger is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# python-alogger is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with python-alogger  If not, see <http://www.gnu.org/licenses/>.
+
 # Anyone using this code as is deserves to be shot. In the left foot.
 # Repeatedly.
 
 """
 
-To Configure this program see alogger-ng.cfg
+To Configure this program see python-alogger.cfg
 
 To customise this to read a different log format
 just implement a method like pbs_to_dict (see below)
