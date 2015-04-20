Karaage-Cluster/python-alogger

commit 5c763c63a41e452f00dfd413162735f5b9fa5732

Various updates
* PBS Pro uses 'project' not 'account'.
* Simplify act_wall_time processing.
* est_wall_time is optional and can be None.

Change-Id: I9c85afb1ffba751165b5f66cd200d4988d7aff23

alogger/parsers/torque.py
@@ -95,28 +95,22 @@ class Parser(BaseParser):
             return None
 
         formatted_data['user'] = data['user']
-        if 'account' in data:
+        if 'project' in data:
+            formatted_data['project'] = data['project']
+        elif 'account' in data:
             formatted_data['project'] = data['account']
 
         formatted_data['jobname'] = data['jobname']
         formatted_data['group'] = data['group']
-        try:
-            formatted_data['act_wall_time'] = \
-                get_in_seconds(data['resources_used.walltime'])
-        except:
-            logger.error(
-                'Failed to parse act_wall_time value: %s'
-                % data['resources_used.walltime'])
-            raise ValueError
 
-        try:
+        formatted_data['act_wall_time'] = \
+            get_in_seconds(data['resources_used.walltime'])
+
+        if 'est_wall_time' in data:
             formatted_data['est_wall_time'] = \
-                get_in_seconds(data['Resource_List.walltime'])
-        except:
-            logger.error(
-                'Failed to parse est_wall_time value: %s'
-                % data['Resource_List.walltime'])
-            raise ValueError
+                get_in_seconds(data.get('Resource_List.walltime'))
+        else:
+            formatted_data['est_wall_time'] = None
 
         formatted_data['exec_hosts'] = \
             [x[:-2] for x in data['exec_host'].split('+')]

alogger/tests/examples/torque.log
@@ -49,3 +49,9 @@
 04/01/2010 23:59:40;S;942351.tango-m.vpac.org;user=blair group=monash account=TestProject1 jobname=Grid_ng2_ queue=run_1_day ctime=1270126375 qtime=1270126375 etime=1270126375 start=1270126780 owner=blair@ng2.vpac.org exec_host=tango108/3 Resource_List.neednodes=tango108 Resource_List.nodect=1 Resource_List.nodes=1 Resource_List.pmem=4000mb Resource_List.walltime=72:00:00 
 04/01/2010 23:59:53;D;942363.tango-m.vpac.org;requestor=philipn@tango.vpac.org
 04/01/2010 23:59:53;E;942363.tango-m.vpac.org;user=philipn group=monash account=pMona0041 jobname=pa_46 queue=run_3_days ctime=1270126773 qtime=1270126773 etime=1270126773 start=1270126775 owner=philipn@tango.vpac.org exec_host=tango028/3 Resource_List.nodect=1 Resource_List.nodes=1 Resource_List.pmem=4000mb Resource_List.walltime=72:00:00 session=5600 end=1270126793 Exit_status=271 resources_used.cput=00:00:17 resources_used.mem=49336kb resources_used.vmem=207332kb resources_used.walltime=00:00:18
+10/07/2014 15:23:19;E;300.zulu.in.vpac.org;user=cwest group=systems account=pSyst0001 jobname=ping.batch queue=batch ctime=1412655504 qtime=1412655504 etime=1412655504 start=1412655505 owner=cwest@zulu.in.vpac.org exec_host=zulu03/1 Resource_List.neednodes=1 Resource_List.nodect=1 Resource_List.nodes=1 Resource_List.walltime=24:00:00 session=25692 total_execution_slots=1 unique_node_count=1 end=1412655799 Exit_status=271 resources_used.cput=00:00:00 resources_used.mem=10324kb resources_used.vmem=496624kb resources_used.walltime=00:04:43 
+12/03/2014 08:53:07;E;4.zulu.in.vpac.org;user=cwest group=systems project=_pbs_project_default jobname=ping.batch queue=workq ctime=1417497605 qtime=1417497605 etime=1417497605 start=1417497606 exec_host=zulu/0 exec_vnode=(zulu:ncpus=1) Resource_List.ncpus=1 Resource_List.nodect=1 Resource_List.nodes=1 Resource_List.place=scatter Resource_List.select=1:ncpus=1 session=29551 end=1417557187 Exit_status=271 resources_used.cpupercent=0 resources_used.cput=00:00:00 resources_used.mem=5932kb resources_used.ncpus=1 resources_used.vmem=255704kb resources_used.walltime=16:33:00 run_count=1
+12/03/2014 09:28:01;E;7.zulu.in.vpac.org;user=cwest group=systems project=_pbs_project_default jobname=ping.batch queue=workq ctime=1417558439 qtime=1417558439 etime=1417558439 start=1417558440 exec_host=zulu03/0*4 exec_vnode=(zulu03:ncpus=4) Resource_List.mpiprocs=4 Resource_List.ncpus=4 Resource_List.nodect=1 Resource_List.nodes=1:ppn=4 Resource_List.place=scatter Resource_List.select=1:ncpus=4:mpiprocs=4 session=28372 end=1417559281 Exit_status=1 resources_used.cpupercent=0 resources_used.cput=00:00:00 resources_used.mem=6020kb resources_used.ncpus=4 resources_used.vmem=259932kb resources_used.walltime=00:08:49 run_count=1
+12/03/2014 09:28:04;E;5.zulu.in.vpac.org;user=cwest group=systems project=_pbs_project_default jobname=ping.batch queue=workq ctime=1417557310 qtime=1417557310 etime=1417557310 start=1417557311 exec_host=zulu01/0+zulu02/0 exec_vnode=(zulu01:ncpus=1)+(zulu02:ncpus=1) Resource_List.ncpus=2 Resource_List.nodect=2 Resource_List.nodes=2 Resource_List.place=scatter Resource_List.select=2:ncpus=1 session=28333 end=1417559284 Exit_status=1 resources_used.cpupercent=0 resources_used.cput=00:00:00 resources_used.mem=6884kb resources_used.ncpus=2 resources_used.vmem=273024kb resources_used.walltime=00:27:41 run_count=1
+12/03/2014 10:51:32;E;8.zulu.in.vpac.org;user=cwest group=systems project=_pbs_project_default jobname=ping.batch queue=workq ctime=1417560372 qtime=1417560372 etime=1417560372 start=1417560372 exec_host=zulu01/0+zulu02/0 exec_vnode=(zulu01:ncpus=1)+(zulu02:ncpus=1) Resource_List.ncpus=2 Resource_List.nodect=2 Resource_List.nodes=2 Resource_List.place=scatter Resource_List.select=2:ncpus=1 Resource_List.walltime=01:00:00 session=28587 end=1417564292 Exit_status=271 resources_used.cpupercent=0 resources_used.cput=00:00:00 resources_used.mem=6872kb resources_used.ncpus=2 resources_used.vmem=273024kb resources_used.walltime=01:00:08 run_count=1
+12/03/2014 11:04:36;E;9.zulu.in.vpac.org;user=cwest group=systems project=_pbs_project_default jobname=ping.batch queue=workq ctime=1417561081 qtime=1417561081 etime=1417561081 start=1417561081 exec_host=zulu01/1+zulu02/1 exec_vnode=(zulu01:ncpus=1)+(zulu02:ncpus=1) Resource_List.ncpus=2 Resource_List.nodect=2 Resource_List.nodes=2 Resource_List.place=scatter Resource_List.pmem=1000mb Resource_List.select=2:ncpus=1 Resource_List.walltime=01:00:00 session=28651 end=1417565076 Exit_status=271 resources_used.cpupercent=0 resources_used.cput=00:00:00 resources_used.mem=6872kb resources_used.ncpus=2 resources_used.vmem=273024kb resources_used.walltime=01:01:21 run_count=1

commit 54e38ffb65c45257bc4cbc06a69faaffc128cf88

Fix est_wall_time processing.
Was broken in 5c763c63a41e452f00dfd413162735f5b9fa5732.

Change-Id: I43e946197b66dedd49599f13a09b866734504246

alogger/parsers/torque.py
@@ -118,7 +118,7 @@ class Parser(BaseParser):
         formatted_data['act_wall_time'] = \
             get_in_seconds(data['resources_used.walltime'])
 
-        if 'est_wall_time' in data:
+        if 'Resource_List.walltime' in data:
             formatted_data['est_wall_time'] = \
                 get_in_seconds(data.get('Resource_List.walltime'))
         else: