4beced3ae36796fb3325e8dd93c5888d853337d6
Brian May <brian@linuxpenguins.xyz>
2019-06-05 17:14:42 +1000

    Update python versions supported

--- setup.py
+++ setup.py
@@ -62,9 +62,7 @@ setup(
         "License :: OSI Approved :: GNU Lesser General Public License v3 "
         "or later (LGPLv3+)",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     keywords="karaage cluster user administration",
